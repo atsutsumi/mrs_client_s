edf00a0e8deeb6a3c6fbd9e4a217efef0c4cc57a
ruby-developer <mieno.masayuki@adniss.jp>
2013-03-26 12:25:46 +0900

    チェックサム値をロジックで生成する対応

--- lib/mrscs/header_helper.rb
+++ lib/mrscs/header_helper.rb
@@ -71,6 +71,9 @@ module Mrscs
       str_bch = "#{str_bch}#{@@bch_config['recv_agency_bit']}"
       str_bch = "#{str_bch}#{@@bch_config['recv_agency_termno']}"
       
+      # チェックサム値を作成
+      str_bch = gen_checksum(str_bch)
+      
       # BCHヘッダをバイナリ化して付与
       bin_bch = str_bch.bit2bin
       
@@ -78,11 +81,51 @@ module Mrscs
     end
     
     #
+    # 引数のBCH文字列からチェックサム値を作成
     #
-    #
+    # ==== Args
+    # _str_bch :: BCHの2進文字列表現
+    # ==== Return
+    # _String_ :: 引数にチェックサム値を挿入した文字列
+    # ==== Raise
     def self.gen_checksum(str_bch)
+      sum = 0
+      # BCHを16桁づつ分割し全て加算
+      0.upto(9) {|index|
+        # チェックサム部分は加算をスキップ
+        next if index == 5
+        tmp = str_bch[index*16, 16]
+        sum = sum + tmp.to_i(2)
+      }
       
+      # 加算結果を32桁の文字列に変換
+      sum = format("%.32b", sum)
+      
+      # 上位16桁が0になるまで上位16桁を下位16桁に加算
+      upper = 0
+      under = 0
+      loop do
+        # 上位16桁を取得
+        upper = sum[0, 16].to_i(2)
+        under = sum[16,16].to_i(2)
+        
+        break if upper == 0
+        
+        # 上位16桁と下位16桁を加算
+        tmp_sum = upper + under
+        
+        # 加算結果を32桁の文字列に変換
+        sum = format("%.32b", sum)
+      end
+      
+      # 計算されたチェックサム値
+      checksum = format("%16b", ~under&0xFFFF)
+      
+      # 引数のBCH文字列にチェックサム値を挿入して返却する
+      str_bch[80,16] = checksum
+      
+      return str_bch
     end
-
+    
   end # HeaderHelper
 end # JmaClient
